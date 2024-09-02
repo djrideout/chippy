ad7fdd271bea24aaade35a217ad3aa22d396926e
D.J. Rideout <djrideoutgit@proton.me>
2024-09-02 16:21:54 -0230

    Install clap and load ROM using command line args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 // Based mainly on the docs at http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 
 use macroquad::prelude::*;
+use clap::Parser;
+
+// Command line arguments
+#[derive(Parser, Debug)]
+struct Args {
+    // The path to the ROM to read
+    #[arg(short, long)]
+    input: String,
+}
 
 #[macroquad::main("chippy")]
 async fn main() {
+    // Load ROM file
+    let _args = Args::parse();
+    let _result = load_file(&_args.input).await;
+    let _rom = match _result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the ROM: {error:?}")
+    };
+
     // Registers
     let mut r_v: [u8; 16] = [0; 16]; // 16 general purpose "Vx" registers (x is 0-F)
     let mut r_i: u16 = 0; // Register "I"
@@ -17,6 +34,11 @@ async fn main() {
 
     // Memory
     let mut mem: [u8; 0x1000] = [0; 0x1000];
+    let mut i = 0x200; // ROM starts at 0x200 in memory
+    for byte in _rom.into_iter() {
+        mem[i] = byte;
+        i += 1;
+    }
 
     loop {
         clear_background(RED);
