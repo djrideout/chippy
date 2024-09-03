b4144ddc51dbdf96ea9ec4bdf0e13e5e6b85c84a
D.J. Rideout <djrideoutgit@proton.me>
2024-09-02 22:37:25 -0230

    Implement some instructions (draw is partially implemented) and printing display to the console This is enough to get the splash screen test from https://github.com/Timendus/chip8-test-suite running

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ struct Args {
     input: String,
 }
 
+const WIDTH: usize = 64;
+const WIDTH_BYTES: usize = WIDTH / 8;
+const HEIGHT: usize = 32;
+
 #[macroquad::main("chippy")]
 async fn main() {
     // Load ROM file
@@ -40,13 +44,69 @@ async fn main() {
         i += 1;
     }
 
+    // Display (64x32 monochrome)
+    let mut display: [u8; WIDTH_BYTES * HEIGHT] = [0; WIDTH_BYTES * HEIGHT];
+
     loop {
-        clear_background(RED);
+        //clear_background(RED);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        //draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+        //draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        // Get opcode
+        let _op: u16 = ((mem[r_pc as usize] as u16) << 8) | mem[(r_pc + 1) as usize] as u16;
+        r_pc += 2;
+
+        // Decode opcode
+        if _op == 0x00E0 {
+            // 00E0 - CLS
+            // Clear the display.
+            for i in 0 .. WIDTH_BYTES * HEIGHT {
+                display[i] = 0;
+            }
+        } else if (_op & !0xFFF) == 0x1000 {
+            // 1nnn - JP addr
+            // Jump to location nnn.
+            r_pc = _op & 0xFFF;
+        } else if (_op & !0xFFF) == 0x6000 {
+            // 6xkk - LD Vx, byte
+            // Set Vx = kk.
+            let _x = ((_op & 0xF00) >> 8) as usize;
+            r_v[_x] = (_op & 0xFF) as u8;
+        } else if (_op & !0xFFF) == 0xA000 {
+            // Annn - LD I, addr
+            // Set I = nnn.
+            r_i = _op & 0xFFF;
+        } else if (_op & !0xFFF) == 0xD000 {
+            // Dxyn - DRW Vx, Vy, nibble
+            // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+            // Sprites are 8 pixels (8 bits/1 byte) wide and from 1 to 15 pixels in height,
+            // So each byte is one row of the sprite.
+            // TODO: Support unaligned sprites
+            let _x = (r_v[((_op & 0xF00) >> 8) as usize] / 8) as usize;
+            let _y = r_v[((_op & 0xF0) >> 4) as usize] as usize;
+            let _n = (_op & 0xF) as usize;
+            let mut unset = false;
+            for i in 0 .. _n {
+                let byte = mem[i + r_i as usize];
+                let display_index = (_y + i) * WIDTH_BYTES + _x;
+                let prev = display[display_index];
+                display[display_index] ^= byte;
+                unset = unset || ((!display[display_index] & prev) > 0);
+            }
+            r_v[0xF] = unset as u8;
+        }
+
+        // Render display
+        for i in 0 .. HEIGHT {
+            for j in 0 .. WIDTH_BYTES {
+                print!("{:08b}", display[i * WIDTH_BYTES + j]);
+            }
+            println!();
+        }
+        println!();
 
         next_frame().await;
     }
