243ae33446b3a9b9a736c0c3a7a7ef503a71c2df
D.J. Rideout <djrideoutgit@proton.me>
2024-09-14 20:42:41 -0230

    Add IBM tests

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -43,3 +43,43 @@ fn test_1_chip8_logo_xo() {
     assert_eq!(pretty_plane(&chip8.planes[0]), pretty_plane(&data::LOGO_PLANE));
     assert_eq!(pretty_plane(&chip8.planes[1]), pretty_plane(&data::EMPTY_PLANE));
 }
+
+#[test]
+fn test_2_ibm_logo_chip() {
+    let mut chip8 = core::build_chip8(core::Target::Chip, 16, data::IBM.to_vec());
+    for _i in 0..data::IBM_FRAME_COUNT {
+        core::run_frame(&mut chip8);
+    }
+    assert_eq!(pretty_plane(&chip8.planes[0]), pretty_plane(&data::IBM_PLANE));
+    assert_eq!(pretty_plane(&chip8.planes[1]), pretty_plane(&data::EMPTY_PLANE));
+}
+
+#[test]
+fn test_2_ibm_logo_super_modern() {
+    let mut chip8 = core::build_chip8(core::Target::SuperModern, 16, data::IBM.to_vec());
+    for _i in 0..data::IBM_FRAME_COUNT {
+        core::run_frame(&mut chip8);
+    }
+    assert_eq!(pretty_plane(&chip8.planes[0]), pretty_plane(&data::IBM_PLANE));
+    assert_eq!(pretty_plane(&chip8.planes[1]), pretty_plane(&data::EMPTY_PLANE));
+}
+
+#[test]
+fn test_2_ibm_logo_super_legacy() {
+    let mut chip8 = core::build_chip8(core::Target::SuperLegacy, 16, data::IBM.to_vec());
+    for _i in 0..data::IBM_FRAME_COUNT {
+        core::run_frame(&mut chip8);
+    }
+    assert_eq!(pretty_plane(&chip8.planes[0]), pretty_plane(&data::IBM_PLANE));
+    assert_eq!(pretty_plane(&chip8.planes[1]), pretty_plane(&data::EMPTY_PLANE));
+}
+
+#[test]
+fn test_2_ibm_logo_xo() {
+    let mut chip8 = core::build_chip8(core::Target::XO, 16, data::IBM.to_vec());
+    for _i in 0..data::IBM_FRAME_COUNT {
+        core::run_frame(&mut chip8);
+    }
+    assert_eq!(pretty_plane(&chip8.planes[0]), pretty_plane(&data::IBM_PLANE));
+    assert_eq!(pretty_plane(&chip8.planes[1]), pretty_plane(&data::EMPTY_PLANE));
+}
