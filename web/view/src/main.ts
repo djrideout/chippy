696dea9276faf6001f1162ffa143093fb8bd4b02
D.J. Rideout <djrideoutgit@proton.me>
2024-10-14 15:21:05 -0230

    Focus the canvas when it is added to the DOM

--- web/view/src/main.ts
+++ web/view/src/main.ts
@@ -7,11 +7,14 @@ let rom = new Uint8Array(await (await fetch('nyancat.ch8')).arrayBuffer());
 
 let chip8 = new Chip8(Target.XO, 30000, rom);
 
-let emuDiv = document.querySelector<HTMLDivElement>("#emulator");
-if (emuDiv) {
-    emuDiv.style.width = `${chip8.get_width() * 5}px`;
-    emuDiv.style.height = `${chip8.get_height() * 5}px`;
-    emuDiv.style.display = 'block';
+let width = chip8.get_width();
+let height = chip8.get_height();
+
+let emuContainer = document.querySelector<HTMLDivElement>('#emulator-container');
+if (emuContainer) {
+    emuContainer.style.width = `${width * 5}px`;
+    emuContainer.style.height = `${height * 5}px`;
+    emuContainer.style.display = 'block';
 }
 
 let keymap = new Keymap([
@@ -40,3 +43,20 @@ overlay?.addEventListener('click', () => {
     overlay.remove();
     frontend.start();
 });
+
+let loadedResolve: (value: unknown) => void;
+let loaded = new Promise((res) => {
+    loadedResolve = res;
+});
+
+let observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        if ([...mutation.addedNodes].some((node) => (node as HTMLElement).tagName === 'CANVAS')) {
+            loadedResolve(true);
+        }
+    }
+});
+observer.observe(emuContainer as Node, { childList: true, subtree: true });
+await loaded;
+observer.disconnect();
+document.querySelector('canvas')?.focus();
